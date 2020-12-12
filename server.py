cb53b60bfe944511553d75791f19f76388a8ed2b
Ashish Kumar <60195105+ashishcssom@users.noreply.github.com>
2020-12-12 18:02:47 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,27 @@
+"""
+.  .     .               .   .                                   .
+|\ |     |               |   |                                   |             o  ,- o
+| \| ,-: |-  . . ;-. ,-: |   | ,-: ;-. ,-: . . ,-: ,-: ,-.   ,-. | ,-: ,-. ,-. .  |  . ,-. ;-.
+|  | | | |   | | |   | | |   | | | | | | | | | | | | | |-'   |   | | | `-. `-. |  |- | |-' |
+'  ' `-` `-' `-` '   `-` '   ' `-` ' ' `-| `-` `-` `-| `-'   `-' ' `-` `-' `-' '  |  ' `-' '
+                                       `-'         `-'                           -'
+"""
+
+from flask import Flask,request
+import pandas as pd
+import json
+import src
+app = Flask(__name__)
+ 
+@app.route('/Request', methods = ['POST','GET'])
+def RequestHandler():
+    """ method to predict commitment """
+    content = request.get_json()
+    Data=pd.DataFrame([content])
+    try:
+        output=src.main(Data,filename="Request")
+    except:
+        output=[]
+    return(output)
+
+app.run(debug=True,threaded=True)
